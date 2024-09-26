4eb1f36e49c9c3e2ddf87472964743954d13dde4
“Swapnil <swapnilkumbhar366@gmail.com>
2024-09-26 21:53:57 +0530

    Created Accordion with ReactBootstrap

---
+++ README.md
@@ -0,0 +1,1 @@
+Created Accordion with ReactBootstrap
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Accordions from './components/Accordion/Accordions'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Accordions/>
+    </>
+  )
+}
+
+export default App
